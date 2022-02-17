1d74a9d9e97497a4e9d541d10d524cc2bcb4c747
zyc <zyc@qq.com>
2022-02-17 16:38:13 +0800

    commit2

--- src/main/java/com/whut/test01.java
+++ src/main/java/com/whut/test01.java
@@ -3,5 +3,6 @@ package com.whut;
 public class test01 {
     public static void main(String[] args) {
         System.out.println("hello git1111");
+        System.out.println("hello git2222");
     }
 }
